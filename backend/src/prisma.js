9ed1d90abcd2b01aeedd4f85b6fe3f4b7033e202
Kenneth Kuo <kennethkuo@Kenneths-MacBook-Pro.local>
2026-01-28 17:53:14 +0700

    Fixed backend

--- backend/src/prisma.js
+++ backend/src/prisma.js
@@ -1,7 +1,15 @@
-require("dotenv").config();
-
+// backend/src/prisma.js
 const { PrismaClient } = require("@prisma/client");
 
-const prisma = new PrismaClient();
+const globalForPrisma = globalThis;
+
+const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    // optional, but nice during dev:
+    log: ["error", "warn"],
+  });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 module.exports = { prisma };
